c121042c71ca7ed258f06babf13ad9c6e7598a84
Drissho12345 <12345drisshoroy@gmail.com>
2024-03-26 20:48:45 +0600

    a new commit

--- src/Component/Book/Book.jsx
+++ src/Component/Book/Book.jsx
@@ -1,12 +1,15 @@
 
 import { CiStar } from "react-icons/ci";
 import { PropTypes } from 'prop-types';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Book = ({book}) => {
-    const {image,type,bookName,tags, author,rating,id} = book;
+    const {image,type,bookName,tags, author,rating,} = book;
     return (
-        <Link to={`/BookBio/${id}`} className="transition border-2 hover:scale-105 border-primary hover:border-secondary border-opacity-30 shadow-xl rounded-3xl">
+        // <Link to={`/BookBio/${id}`} className="transition border-2  hover:scale-105 border-primary hover:border-secondary border-opacity-30 shadow-xl rounded-3xl">
+            
+        // </Link>
+        <div>
             <div>
                 <div className="card">
                     <figure className="px-10 m-5  rounded-3xl bg-[#F3F3F3] pt-10">
@@ -34,7 +37,7 @@ const Book = ({book}) => {
                     </div>
                 </div>
             </div>
-        </Link>
+        </div>
     );
 };
 
